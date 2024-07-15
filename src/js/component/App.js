26be23c290b53493a1a6ac05f2b3f4b724445219
S-Sotelo <168486323+S-Sotelo@users.noreply.github.com>
2024-07-15 20:13:59 +0000

    todo-final

---
+++ src/js/component/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+import React from "react";
+import { TodoWrapper } from "./TodoWrapper";
+import "../../styles/index.css";
+
+function App() {
+	return (
+		<div className="home-header"> 
+			<TodoWrapper />
+		</div>
+	);
+}
+
+export default App;
